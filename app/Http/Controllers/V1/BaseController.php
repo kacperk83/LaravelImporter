86f2835534b9b1a5fb8abbebc17ab1602aae63e1
Kacper Kowalski <kacperk83@gmail.com>
2018-02-11 20:43:25 +0100

    Added flexible query param handling +validation (needs some testing though..)

--- app/Http/Controllers/V1/BaseController.php
+++ app/Http/Controllers/V1/BaseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\V1;
 
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 /**
  * Class BaseController
@@ -20,9 +21,28 @@ class BaseController extends Controller
     protected $request;
 
     /**
+     * @var array $cleanQueryParams
+     */
+    protected $cleanQueryParams = [];
+
+    /**
      * @var array $queryParams
      */
-    protected $queryParams;
+    protected $queryParams = [
+
+        'expand' => [
+            'rules' => 'array',
+            'default' => []
+        ],
+        'limit' => [
+            'rules' => 'int',
+            'default' => 10
+        ],
+        'offset' => [
+            'rules' => 'int',
+            'default' => 0
+        ]
+    ];
 
     /**
      * BaseController constructor.
@@ -32,18 +52,53 @@ class BaseController extends Controller
     public function __construct(Request $request)
     {
         $this->request = $request;
+
     }
 
     /**
-     * @deprecated
+     * processQueryParams
      *
-     * @return array
+     * Here we collect the possible queryparams. If there is no value provided for a queryparam
+     * we take the default one. Finally we validate every parameter.
      */
-    public function getExpands()
+    protected function processQueryParams()
     {
-        return $this->request->query('expand', []);
+        //collect input data
+        $data = [];
+        $rules = [];
+        foreach (array_keys($this->queryParams) as $param) {
+            $data[$param] =
+                $this->request->query($param, $this->queryParams[$param]['default']);
+            $rules[$param] = $this->queryParams[$param]['rules'];
+        }
+
+        Validator::make($data, $rules)->validate();
+
+        $this->cleanQueryParams = $data;
     }
 
-    //@todo: ophalen query parameters zoals: expand, limit en offset
-    //@todo: + valideren + plaatsen in $queryParams
+    /**
+     * getCleanQueryParams
+     */
+    protected function getCleanQueryParams()
+    {
+        return $this->cleanQueryParams;
+    }
+
+    /**
+     * @param string $queryParam
+     * @param string $rules
+     */
+    protected function updateQueryParamRules(string $queryParam, string $rules)
+    {
+        $this->queryParams[$queryParam]['rules'] = $rules;
+    }
+
+    /**
+     * @param array $params
+     */
+    protected function deleteQueryParams(array $params)
+    {
+        $this->queryParams = array_diff($params, $this->queryParams);
+    }
 }
